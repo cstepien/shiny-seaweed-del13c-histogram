778bf4fb2f65321cd5777b80bcf556ff279814d2
Courtney Stepien <courtney.stepien@gmail.com>
2016-09-24 17:47:06 -0500

    Added my own data in, made a reactive histogram by Phylum

--- app.R
+++ app.R
@@ -10,19 +10,29 @@ ui <- fluidPage(
                      selected = c("Chlorophyta", "Rhodophyta", 
                                   "Ochrophyta", "Tracheophyta")), 
   actionButton(inputId = "go",
-               label = "Update"),
+               label = "Plot histogram"),
   plotOutput(outputId = "hist"),
   verbatimTextOutput(outputId = "stats")
 )
 
 server <- function(input, output) {
   data <- eventReactive(input$go, 
-                        {rnorm(input$isotope)})
+                        {filter(rawdata, division %in% input$taxa)})
   output$hist <- renderPlot({
-    title <- "Histogram of Seaweed Carbon-13 Isotope Values" 
-    hist(rnorm(data()), main = title)})
-  output$stats <- renderPrint({
-    summary(rnorm(data()))})
+    ggplot(data(), aes(x = del13c)) + geom_histogram(binwidth = 1) + xlim(-40,0) + ylim(0,110) +
+      geom_vline(xintercept = -30, linetype = "dotted", size = 1) +
+      xlab(expression(paste("\nMean species ", delta^{13}, "C ", "(\u2030)"))) +
+      ylab("Count") +
+      theme(legend.key = element_blank(), panel.grid.major.y = element_line(color = "gray"), 
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.text.x = element_text(size=20, color = "black"), 
+            axis.text.y = element_text(size=20, color = "black"),
+            axis.title.x = element_text(size=20), 
+            axis.title.y = element_text(size=20))
+    })
+  #output$stats <- renderPrint({
+   # summary(mode(data()))})
 }
 
 
