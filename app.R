00b600bdbe639e0868f04afa6332daacbd6d0e34
Courtney Stepien <courtney.stepien@gmail.com>
2016-09-24 15:50:30 -0500

    add reactive expression (data input) to app.R

--- app.R
+++ app.R
@@ -4,13 +4,21 @@ ui <- fluidPage(
   sliderInput(inputId = "isotope",
               label = "Choose a Carbon-13 isotope value", 
               value = 5, min = 1, max = 100),
-  plotOutput(outputId = "hist")
+  plotOutput(outputId = "hist"),
+  verbatimTextOutput(outputId = "stats")
 )
 
 server <- function(input, output) {
+  data <- reactive({
+    rnorm(input$isotope)
+    })
   output$hist <- renderPlot({
     title <- "Histogram of Seaweed Carbon-13 Isotope Values" 
-    hist(rnorm(input$isotope), main = title)})
+    hist(rnorm(data()), main = title)})
+  output$stats <- renderPrint({
+    summary(rnorm(data()))})
 }
+
+
 
 shinyApp(ui = ui, server = server)
