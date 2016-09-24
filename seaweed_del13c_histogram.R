72de4b4505a990bfba0745fb74a7f0fd486e9e96
Courtney Stepien <courtney.stepien@gmail.com>
2016-09-24 12:27:59 -0500

    add shiny template, with histogram

--- seaweed_del13c_histogram.R
+++ seaweed_del13c_histogram.R
@@ -1,7 +1,16 @@
 library(shiny)
 
-ui <- fluidPage()
+ui <- fluidPage(
+  sliderInput(inputId = "isotope",
+              label = "Choose a Carbon-13 isotope value", 
+              value = 5, min = 1, max = 100),
+  plotOutput(outputId = "hist")
+)
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    title <- "Histogram of Seaweed Carbon-13 Isotope Values" 
+    hist(rnorm(input$isotope), main = title)})
+}
 
 shinyApp(ui = ui, server = server)
