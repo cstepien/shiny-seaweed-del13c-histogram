8517dab668b12ad91dd05e8247cfb5429d930896
Courtney Stepien <courtney.stepien@gmail.com>
2016-09-24 16:31:19 -0500

    Made reactive expression dependent on Update button

--- app.R
+++ app.R
@@ -4,14 +4,15 @@ ui <- fluidPage(
   sliderInput(inputId = "isotope",
               label = "Choose a Carbon-13 isotope value", 
               value = 5, min = 1, max = 100),
+  actionButton(inputId = "go",
+               label = "Update"),
   plotOutput(outputId = "hist"),
   verbatimTextOutput(outputId = "stats")
 )
 
 server <- function(input, output) {
-  data <- reactive({
-    rnorm(input$isotope)
-    })
+  data <- eventReactive(input$go, 
+                        {rnorm(input$isotope)})
   output$hist <- renderPlot({
     title <- "Histogram of Seaweed Carbon-13 Isotope Values" 
     hist(rnorm(data()), main = title)})
